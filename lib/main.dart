f984b844dc9c8c754239eb5cdd976620f6716aa0
Tania Joseph <taniajoseph37@gmail.com>
2020-06-05 01:37:35 +0530

    Make some music.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void play(int n){
+    final player = AudioCache();
+    player.play('note$n.wav');
+  }
+
+  Widget buildKey(int n, Color color){
+    return Expanded(
+      child: FlatButton(
+        onPressed: (){
+          play(n);
+        },
+        color: color,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
-        ),
+
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+
+               buildKey(1, Colors.purple),
+               buildKey(2, Colors.indigo),
+               buildKey(3, Colors.blue),
+               buildKey(4, Colors.green),
+               buildKey(5, Colors.yellow),
+               buildKey(6, Colors.orange),
+               buildKey(7, Colors.red),
+
+
+              ],
+            ),
+          )
+        ), 
       ),
     );
   }
 }
+
